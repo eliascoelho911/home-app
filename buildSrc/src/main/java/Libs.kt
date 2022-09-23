072932bdb8041c3932de596558579165cc00b91f
elias <elias_coelho911@hotmail.com>
2022-09-23 17:05:45 -0300

    Initial commit

---
+++ buildSrc/src/main/java/Libs.kt
@@ -0,0 +1,12 @@
+object Libs {
+    // Compose
+    const val composeUi = "androidx.compose.ui:ui:${Versions.compose}"
+    const val composeUiToolingPreview = "androidx.compose.ui:ui-tooling-preview:${Versions.compose}"
+    const val composeUiTooling = "androidx.compose.ui:ui-tooling:${Versions.compose}"
+    const val composeMaterial3 = "androidx.compose.material3:material3:${Versions.composeMaterial3}"
+    const val activityCompose = "androidx.activity:activity-compose:${Versions.activityCompose}"
+
+    // Android
+    const val lifecycleRuntimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleRuntimeKtx}"
+    const val coreKtx = "androidx.core:core-ktx:${Versions.androidCoreKtx}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,21 @@
+object Versions {
+    // Kotlin
+    const val kotlin = "1.7.10"
+
+    // Compose
+    const val compose = "1.2.1"
+    const val composeCompiler = "1.3.0"
+    const val composeMaterial3 = "1.0.0-beta03"
+    const val activityCompose = "1.6.0"
+
+    // Android
+    const val androidCoreKtx = "1.9.0"
+    const val lifecycleRuntimeKtx = "2.5.1"
+    const val androidJUnitExt = "1.1.3"
+
+    // JUnit
+    const val junit = "4.13.2"
+
+    // Gradle
+    const val gradle = "7.3.0"
+}
